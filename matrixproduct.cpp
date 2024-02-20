1e3d847a528a98c1d881ba6bd2ceae97e5ba4523
Vasco Moutinho de Oliveira <up202108881@up.pt>
2024-02-20 16:37:35 +0000

    Added OnMultLine and script to test it

--- matrixproduct.cpp
+++ matrixproduct.cpp
@@ -58,21 +58,21 @@ void OnMult(int m_ar, int m_br)
 	double t = (double)(Time2 - Time1) / CLOCKS_PER_SEC;
 	sprintf(st, "Time: %3.3f seconds\n", t);
 	cout << st;
-
+	/*
 	fstream file;
 	file.open("single_mult_cpp.csv", ios::app);
 
 	file << m_ar << "," << t << ",";
 
-	file.close();
+	file.close(); */
 
 	// display 10 elements of the result matrix to verify correctness
-	/*cout << "Result matrix: " << endl;
+	cout << "Result matrix: " << endl;
 	for(i=0; i<1; i++)
 	{	for(j=0; j<min(10,m_br); j++)
 			cout << phc[j] << " ";
 	}
-	cout << endl; */
+	cout << endl; 
 
     free(pha);
     free(phb);
@@ -84,7 +84,72 @@ void OnMult(int m_ar, int m_br)
 // add code here for line x line matriz multiplication
 void OnMultLine(int m_ar, int m_br)
 {
-    
+    	
+	SYSTEMTIME Time1, Time2;
+	
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+	
+
+		
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++){
+			pha[i*m_ar + j] = (double)1.0;
+			phc[i*m_ar + j] = (double)0.0;
+		}
+
+
+
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+
+    Time1 = clock();
+
+	for(i=0; i<m_ar; i++)
+	{	for(j=0; j<m_br; j++)
+		{	temp = 0;
+			for (k=0; k<m_ar; k++)
+			{
+				temp = pha[i*m_ar + j] * phb[j*m_br + k];
+				phc[i*m_ar + k] += temp;
+			}
+		}
+	}
+
+	Time2 = clock();
+	double t = (double)(Time2 - Time1) / CLOCKS_PER_SEC;
+	sprintf(st, "Time: %3.3f seconds\n", t);
+	cout << st;
+	
+	fstream file;
+	file.open("single_mult_line_cpp.csv", ios::app);
+
+	file << m_ar << "," << t << ",";
+
+	file.close(); 
+
+
+	/*
+	// display 10 elements of the result matrix to verify correctness
+	cout << "Result matrix: " << endl;
+	for(i=0; i<1; i++)
+	{	for(j=0; j<min(10,m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl; 
+	*/
+
+    free(pha);
+    free(phb);
+    free(phc);
     
 }
 
@@ -202,7 +267,7 @@ int main (int argc, char *argv[])
 
 	ret = PAPI_destroy_eventset( &EventSet );
 	if ( ret != PAPI_OK )
-		std::cout << "FAIL destroy" << endl;
+		std::cout << "FAIL destroy" << endl;
 
 }
 */
---
+++ testOnMultLine.cpp
@@ -0,0 +1,76 @@
+#include "matrixproduct.cpp"
+
+int main (int argc, char *argv[])
+{
+
+	char c;
+	
+	int EventSet = PAPI_NULL;
+  	long long values[2];
+  	int ret;
+	
+    // Start running
+	ret = PAPI_library_init( PAPI_VER_CURRENT );
+	if ( ret != PAPI_VER_CURRENT )
+		std::cout << "FAIL" << endl;
+
+	ret = PAPI_create_eventset(&EventSet);
+		if (ret != PAPI_OK) cout << "ERROR: create eventset" << endl;
+
+	ret = PAPI_add_event(EventSet,PAPI_L1_DCM );
+	if (ret != PAPI_OK) cout << "ERROR: PAPI_L1_DCM" << endl;
+
+	ret = PAPI_add_event(EventSet,PAPI_L2_DCM);
+	if (ret != PAPI_OK) cout << "ERROR: PAPI_L2_DCM" << endl;
+
+	// Open file
+	ofstream file;
+	file.open("single_mult_line_cpp.csv");
+
+	file << "size,time,l1,l2" << endl;
+
+	file.close();
+
+    for (int i = 600; i <= 3000; i += 400) {
+        for (int j = 0; j < 2; j++) {
+
+            // Start counting
+            ret = PAPI_start(EventSet);
+            if (ret != PAPI_OK) cout << "ERROR: Start PAPI" << endl;
+
+            OnMultLine(i, i);
+
+            ret = PAPI_stop(EventSet, values);
+            
+            if (ret != PAPI_OK) cout << "ERROR: Stop PAPI" << endl;
+            printf("L1 DCM: %lld \n",values[0]);
+            printf("L2 DCM: %lld \n",values[1]);
+
+            ret = PAPI_reset( EventSet );
+            
+            if ( ret != PAPI_OK )
+                std::cout << "FAIL reset" << endl;
+			
+			fstream file2;
+			file2.open("single_mult_line_cpp.csv", ios::app);
+
+			file2 << values[0] << "," << values[1] << endl;
+
+			file2.close();
+        }
+    }
+
+    // Stop running
+    ret = PAPI_remove_event( EventSet, PAPI_L1_DCM );
+	if ( ret != PAPI_OK )
+		std::cout << "FAIL remove event" << endl; 
+
+	ret = PAPI_remove_event( EventSet, PAPI_L2_DCM );
+	if ( ret != PAPI_OK )
+		std::cout << "FAIL remove event" << endl; 
+
+	ret = PAPI_destroy_eventset( &EventSet );
+	if ( ret != PAPI_OK )
+		std::cout << "FAIL destroy" << endl;
+
+}
